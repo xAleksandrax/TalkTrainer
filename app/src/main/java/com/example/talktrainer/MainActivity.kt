38c878d9ea399e5466118edc946672827614091e
xAleksandrax <aleksandramodrzejewska2001@gmail.com>
2023-04-02 00:19:19 +0200

    login and register interface

--- app/src/main/java/com/example/talktrainer/MainActivity.kt
+++ app/src/main/java/com/example/talktrainer/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.talktrainer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnLogIn: Button;
+    private lateinit var btnSignUp: Button;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogIn = findViewById(R.id.login_button)
+        btnSignUp = findViewById(R.id.signin_button)
+
+        btnLogIn.setOnClickListener{
+            val intent = Intent(this, LogIn::class.java)
+            startActivity(intent)
+        }
+
+        btnSignUp.setOnClickListener{
+            val intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+        }
     }
 }
