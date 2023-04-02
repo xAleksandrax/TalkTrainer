a0c28222429e640fa8e29ca79987161f4d9fce38
xAleksandrax <aleksandramodrzejewska2001@gmail.com>
2023-04-02 14:06:09 +0200

    login and register activity added

--- app/src/main/java/com/example/talktrainer/LogIn.kt
+++ app/src/main/java/com/example/talktrainer/LogIn.kt
@@ -1,11 +1,46 @@
 package com.example.talktrainer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LogIn : AppCompatActivity() {
+
+    private lateinit var loginBtn: Button
+    private lateinit var editUser: EditText
+    private lateinit var editPword: EditText
+    private lateinit var dbh: DBHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        loginBtn = findViewById(R.id.login_button)
+        editUser = findViewById(R.id.username)
+        editPword = findViewById(R.id.password)
+        dbh = DBHelper(this)
+
+        loginBtn.setOnClickListener{
+            val useredtxt = editUser.text.toString()
+            val pwordedtxt = editPword.text.toString()
+
+            if (TextUtils.isEmpty(useredtxt) || TextUtils.isEmpty(pwordedtxt)){
+                Toast.makeText(this,"Add Username & Password", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val checkUser = dbh.checkUserPassword(useredtxt, pwordedtxt)
+                if (checkUser==true){
+                    Toast.makeText(this,"Login successful!",Toast.LENGTH_SHORT).show()
+                    val intent = Intent(applicationContext, FirstPage::class.java)
+                    startActivity(intent)
+                }
+                else{
+                    Toast.makeText(this,"Wrong Username & Password",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/talktrainer/SignIn.kt
+++ app/src/main/java/com/example/talktrainer/SignIn.kt
@@ -1,11 +1,55 @@
 package com.example.talktrainer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SignIn : AppCompatActivity() {
+
+    private lateinit var uname: EditText
+    private lateinit var pword: EditText
+    private lateinit var cpword: EditText
+    private lateinit var signupBtn: Button
+    private lateinit var db: DBHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        uname = findViewById(R.id.username1)
+        pword = findViewById(R.id.password1)
+        cpword = findViewById(R.id.confirm_password1)
+        signupBtn = findViewById(R.id.signin_button)
+        db = DBHelper(this)
+
+        signupBtn.setOnClickListener{
+            val unametxt = uname.text.toString()
+            val pwordtxt = pword.text.toString()
+            val cpwordtxt = cpword.text.toString()
+            val savedata = db.insertdata(unametxt, pwordtxt)
+
+            if (TextUtils.isEmpty(unametxt) || TextUtils.isEmpty(pwordtxt) || TextUtils.isEmpty(cpwordtxt)){
+                Toast.makeText(this,"Add Username, Password & Confirm Password",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                if (pwordtxt.equals(cpwordtxt)){
+                    if (savedata==true){
+                        Toast.makeText(this,"Success!",Toast.LENGTH_SHORT).show()
+                        val intent = Intent(applicationContext, LogIn::class.java)
+                        startActivity(intent)
+                    }
+                    else{
+                        Toast.makeText(this,"User already exists",Toast.LENGTH_SHORT).show()
+                    }
+                }
+                else{
+                    Toast.makeText(this,"Password not match",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
